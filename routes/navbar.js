984f86dc2da42bfb8c517d9a17860104b2236d46
EnriqueF <kike.fvla@gmail.com>
2020-04-03 20:14:00 -0600

    commit para jalar index

---
+++ routes/navbar.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var router = express.Router();
+
+res.render('navbar', { pagination: '1, 2, 3, 4, 5' });
--- routes/readAnsweredCuestionario.js
+++ routes/readAnsweredCuestionario.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//var fetch = require("node-fetch");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('read-answered-cuestionario', { title: 'Titulo Cuestionario' });
+  //fetch("http://localhost/api/answres/10")
+  //.then(function(response) {return response.json()})
+  //.then(function(json) {
+    const data = {title: "Mi primer cuestionario", pagination: "3"};
+    res.render('read-answered-cuestionario', data);
+  //})
+
+  //res.render('read-answered-cuestionario', { title: 'Titulo Cuestionario' });
 });
 
-module.exports = router;
+module.exports = router; 
