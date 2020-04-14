1bded7e5e2e518c3029822b2b055bdd5ec23e467
epicLevi <tomas.levi@gmail.com>
2020-04-14 02:07:27 -0500

    Full functionality on answering cuestionarios

---
+++ public/js/answerCuestionario.public.js
@@ -0,0 +1,49 @@
+$(document).ready(() => {
+    $('#enviar-btn').on('click', sendCuestionario);
+});
+
+function sendCuestionario() {
+    var json = $('#object').val();
+    var cuestionario = JSON.parse(json);
+    
+    cuestionario.preguntaList.forEach(pregunta => {
+        respuesta = {
+            idRespuesta: 0,
+            idLlenado: 0
+        }
+
+        if (pregunta.isOpen) {
+            respuesta.textoRespuesta = $(`#answer-${pregunta.idPregunta}`).val();
+        } else {
+            respuestaOpcionList = [];
+
+            pregunta.opcionList.forEach(opcion => {
+                respuestaOpcion = {
+                    idOpcion: opcion.idOpcion,
+                    opcionText: opcion.nombre,
+                    selecionado: $(`#answer-${pregunta.idPregunta}-${opcion.idOpcion}`).is(':checked')
+                }
+
+                respuestaOpcionList.push(respuestaOpcion);
+            });
+
+            respuesta.respuestaOpcionList = respuestaOpcionList;
+        }
+
+        pregunta.respuesta = respuesta;
+    });
+
+    let data = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(cuestionario)
+    }
+
+    console.log('Now POSTing...');
+    console.log(data);
+    console.log(data.body);
+    
+
+    fetch('/answer', data)
+    .then(response => console.log(response));
+}
--- routes/answerCuestionario.js
+++ routes/answerCuestionario.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 var session = require('express-session');
 router.use(session({secret: 'secret'}));
 var sess;
+var cuestionario;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET cuestionar.io answering page. */
+router.get('/:codigo', function(req, res, next) {
   sess = req.session;
-  res.render('answer-cuestionario', { title: 'Título Cuestionario' });
+
+  let code = req.params.codigo;
+
+  fetch(`http://localhost:8080/rest/service/cuestionarioConsultable/${code}`)
+  .then(response => response.json())
+  .then(response => {
+    cuestionario = response;
+    cuestionario.usuario = sess.username;
+    cuestionario.preguntaList.forEach(pregunta => {
+      switch (pregunta.tipoPregunta) {
+        case 'Pregunta Abierta':
+          pregunta.isOpen = true;
+          break;
+        case 'Pregunta Opcion Multiple':
+          pregunta.isMultipleOption = true;
+          break;
+        case 'Pregunta Seleccion Multiple':
+          pregunta.isMultipleSelection = true;
+          break;
+      }
+    });
+    cuestionario.object = JSON.stringify(cuestionario);
+
+    return res.render('answer-cuestionario', cuestionario);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  console.log('Received in POST:');
+  console.log(req.body);
+  console.log('Now POSTing...');
+
+  var resquestObject = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(req.body)
+  }
+
+  fetch('http://localhost:8080/rest/service/respuestas', requestObject)
+  .then(response => {
+    console.log(response);
+    res.send(response);
+  });
 });
 
 module.exports = router;
