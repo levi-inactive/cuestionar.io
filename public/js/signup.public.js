21205681bac9db714ef2af60a66a36373402e4f1
EnriqueF <kike.fvla@gmail.com>
2020-04-14 00:28:01 -0500

    Signup listo

---
+++ public/js/signup.public.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
+
+    var btnCreateUser = document.getElementById("btnCreateUser");
+
+    
+});
+
+function createUser(){
+
+}
